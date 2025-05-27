4302743afbedb1a1aaee408dda0d06ae44e0c51d
OleSveberg <32804894+OleSveberg@users.noreply.github.com>
2025-05-27 15:29:57 +0200

    changed to normal buttons, added dynamic hue rotation, fixed narrow window view

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const iconText = document.getElementById("icon-text")
-const iconButtonHolder = document.getElementById("guess-form");
+const guessForm = document.getElementById("guess-form");
 const numberOfAlternatives = 8;
 const guessingTime = 10_000;
+let gameState = "stopped";
 let correctIcon;
 let currentScore = 0;
 let highscore = localStorage.getItem("wtmi_highscore") || 0;
@@ -13,35 +14,46 @@ let timeRemaining = 0;
 let timeRemainingBar = document.getElementById("time-bar");
 
 // random page theme
-let randomHue = Math.random() * 360;
+let randomHue = Math.floor(Math.random() * 360);
 document.documentElement.style.setProperty("--random-hue", `${randomHue}deg`);
+setInterval(hueRotate, 3000);
+function hueRotate() {
+  randomHue = (randomHue + 1) % 360;
+  document.documentElement.style.setProperty("--random-hue", `${randomHue}deg`);
+}
 
 function startGame() {
+  gameState = "playing";
+
+  // remove previous correct icon
+  if (correctIcon != null) {
+    document.getElementById(`guess-button-${correctIcon}`).classList.remove("correct");
+  }
+
   // reset score
   currentScore = 0;
   document.getElementById("score").innerText = currentScore;
-
   document.getElementById("start-button").hidden = true;
   newQuestion();
+
+  for (const element of guessForm.children) {
+    element.classList.add("clickable");
+  }
 }
 function stopGame() {
+  gameState = "stopped";
   clearInterval(countDownTime);
 
-  // remove icon buttons functionality
-  document.querySelectorAll("input[name='guess-buttons']")
-  .forEach(button => {
-    button.removeEventListener("change", handleGuess)
-  });
-  document.querySelectorAll(".guess-icon").forEach(label => {
-    label.classList.remove("clickable");
-  });
+  for (const element of guessForm.children) {
+    element.classList.remove("clickable");
+  }
 
   // show start / retry button
   document.getElementById("start-button").value = "Try again";
   document.getElementById("start-button").hidden = false;
 
   // show correct option
-  document.getElementById(`label-for-guess-button-${correctIcon}`).classList.add("correct");
+  document.getElementById(`guess-button-${correctIcon}`).classList.add("correct");
 
   // update highscore
   if (currentScore > highscore) {
@@ -56,58 +68,33 @@ function incrementTimer() {
   if (timeRemaining <= 0) stopGame();
 }
 function newQuestion() {
-  let iconList = iconListAll; // choose icon set
+  let newIconList = iconListAll; // choose icon set
   let selectedList = [];
   correctIcon = Math.floor(Math.random() * numberOfAlternatives);
 
   // select icons to show the player
   for (i = 0; i < numberOfAlternatives; i++) {
-    let rand = Math.floor(Math.random() * iconList.length);
-    selectedList.push(iconList[rand]);
-    iconList.splice(rand, 1);
+    let rand = Math.floor(Math.random() * newIconList.length);
+    selectedList.push(newIconList[rand]);
+    newIconList.splice(rand, 1);
   }
-  generateButtons(correctIcon, selectedList);
+  
+  // load icon name and guessing alternative icons
+  iconText.innerText = selectedList[correctIcon];
+  for (i = 0; i < selectedList.length; i++) {
+    document.getElementById(`guess-button-${i}`).value = selectedList[i];
+  }
 
   // question timer
   clearInterval(countDownTime);
   timeRemaining = guessingTime;
   timeRemainingBar.style.width = '100%';
   countDownTime = setInterval(incrementTimer, 50);
-  
 }
-function generateButtons(correct, list) {
-  iconText.innerText = list[correct];
 
-  const fragment = document.createDocumentFragment();
-  for (i = 0; i < list.length; i++) {
-    const input = document.createElement("input");
-    input.type = "radio";
-    input.name = "guess-buttons";
-    input.id = `guess-button-${i}`;
-    input.value = i;
-    input.hidden = true;
-    fragment.appendChild(input);
+function handleGuess(value) {
+  if (gameState != "playing") return;
 
-    const label = document.createElement("label");
-    label.htmlFor = `guess-button-${i}`;
-    label.id = `label-for-guess-button-${i}`
-    label.classList.add("guess-icon", "material-icons")
-    label.innerText = list[i];
-    fragment.appendChild(label);
-  }
-  iconButtonHolder.innerHTML = "";
-  iconButtonHolder.appendChild(fragment);
-
-  document.querySelectorAll("input[name='guess-buttons']")
-  .forEach(button => {
-    button.addEventListener("change", handleGuess)
-  });
-  document.querySelectorAll(".guess-icon").forEach(label => {
-    label.classList.add("clickable");
-  });
-}
-function handleGuess(event) {
-  value = event.target.value;
   if (value == correctIcon) {
     currentScore++;
     document.getElementById("score").innerText = currentScore;
